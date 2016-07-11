80ed6af983436bc12cfc24518d024e0abbc38354
aj-bobo <aj.bobo@domo.com>
2016-07-11 17:44:04 -0600

    First commit - just Hello World

---
+++ src/test/java/com/trinova/TestspringappApplicationTests.java
@@ -0,0 +1,18 @@
+package com.trinova;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestspringappApplication.class)
+@WebAppConfiguration
+public class TestspringappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
